920356b29ada6171ea61ab4faa7179075d939342
swetabharti179 <swetabharti783@gmail.com>
2025-03-22 10:08:32 +0530

    My first day learning ( project setup ,hello world program,eclpise shortcut)

---
+++ src/javabasic/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package javabasic;
+//IDE(idhar development easy hai)
+public class MyFirstProgram {
+public static void main(String[] args) {
+System.out.println("Welcom to Java World");
+System.out.println("My First program");
+
+}
+
+}
