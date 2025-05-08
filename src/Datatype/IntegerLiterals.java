f060707c3ff5d2bbbbdb88fd65c4f58362938f53
swetabharti179 <swetabharti783@gmail.com>
2025-05-08 19:12:42 +0530

    datatype lecture

---
+++ src/Datatype/IntegerLiterals.java
@@ -0,0 +1,25 @@
+package Datatype;
+
+public class IntegerLiterals {
+	public static void main(String[] args) {
+		int a=0B100;
+		System.out.println(a);
+		int b=045;
+		System.out.println(b);
+		int c=0X44;
+		System.out.println(c);
+		int d=10000000;
+		int e=1_00_000_000;
+		long f=62_00_20_37_34L;
+		float g=99.99_99_99_99_99F;
+		System.out.println("'Sweta'");
+		System.out.println("\'Sweta\'");
+		System.out.println("\"Sweta\"");
+		System.out.println("\\");
+		System.out.println("\\\\\\");
+		System.out.println("\\\\\\\\\\");//escape sequence
+
+		
+	}
+
+}
